7f7c97c86662c17f0c499474c6c4706d530494d0
NipunBasnayake <nipunsathsara1999@gmail.com>
2025-07-26 23:56:47 +0530

    enhance parameters to get better colours

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from turtle import *
 from colorsys import *
 
-# Setup
+bgcolor('black')
+speed(0)
+pensize(2)
+hideturtle()
 penup()
 setposition(50, -50)
 pendown()
-speed(0)
-bgcolor('black')
-pensize(2)
 
-n = 100
-h = 0
+h = 0.0 
+num_shapes = 120
+sides = 4
 
-for i in range(120):
-    for j in range(4):
-        r, g, b = hsv_to_rgb(h, 1, 1)
+for i in range(num_shapes):
+    for j in range(sides):
+        r, g, b = hsv_to_rgb(h % 1.0, 1, 1)
         color(r, g, b)
-        h += 0.003
+        h += 0.005
         circle(40 + j * 5, 90)
         forward(250)
         left(90)
     right(10)
 
-hideturtle()
 done()
